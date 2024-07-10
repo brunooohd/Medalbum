d4a0b51c7a4e28a719b657155b5499b1cf4ed02c
brunooohd <email@blahblah.com>
2024-07-10 08:29:22 -0300

    foi feito a sessão do live

---
+++ Medalbum/Views/LiveView.swift
@@ -0,0 +1,18 @@
+//
+//  LiveView.swift
+//  Medalbum
+//
+//  Created by Bruno Dias on 05/07/24.
+//
+
+import SwiftUI
+
+struct LiveView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LiveView()
+}
